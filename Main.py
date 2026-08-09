4fb3af99bcbb59179cabbce1a76ccb5d56afb3f2
若杉京佑 <loudbone.k17@gmail.com>
2026-08-09 12:40:31 +0900

    nyuuryoku

--- Main.py
+++ Main.py
@@ -6,5 +6,4 @@ name=st.text_input("あなたの名前を入力してください")
 if st.button("名前を保存"):
     st.session_state.user_name=name
     st.success("名前を保存しました！")
-st.write(f"現在保存されている名前:{st.session.state.name}")
-       
+st.write(f"現在保存されている名前:{st.session_state.name}")
